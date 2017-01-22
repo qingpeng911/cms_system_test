3680f9f3f94c9779ed10db94c8b22fd76077d5de
qIng.peng <qingpeng911@163.com>
2017-01-22 13:58:34 +0800

    测试

--- src/java/com/tjpcms/common/CL.java
+++ src/java/com/tjpcms/common/CL.java
@@ -7,10 +7,10 @@ public class CL {
 	public static final String DB="tjpcms";//网站数据库名
 
 	public static final String WZMC="tjpcms";//网站名称
-	public static final String GGY="这是一条广告语";//广告语
+	public static final String GGY="广告";//广告语
 	public static final String GGY2="CMS测试";//广告语
 	public static final String YUMING="127.0.0.1";//域名
-	public static final String META_DES="tjpcms是一套基于java的cms解决方案，开源免费。其独有的实时配置增删改查的功能，是其区别于同类cms的最大特点，也是最大优势，极大减少了重复劳动。懂jsp即可实现快速建站、静态化等，学习成本极低。文档齐全，支持有力，快来试试吧！";
+	public static final String META_DES="meta描述文字";
 
 	public static final String ses_admin= "ses_admin";
 	public static final String ses_usr= "ses_usr";
